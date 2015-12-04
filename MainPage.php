7d70bb32e75927f98b58c09d4048ae987cff3ebd
NotRickyy <rickyau.9@gmail.com>
2015-12-04 15:54:36 -0500

    fsd
    sdf

---
+++ MainPage.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>
+            Main
+        </title>
+        
+    <!--    <link href="styles.css" rel="stylesheet" type="text/css" />		-->
+    <!--	<script src = "Lab8.js" type ="text/javascript"></script>		-->
+    
+    </head>
+
+    <body>
+         <center>
+    
+             <h1> INSANE CROW'S USED CAR LOT </h1>
+			 
+				<img src="logo.jpg" alt="Insane Crow's car lot" title="Caw caw sales, caw caw cars"/>
+				<p/>
+				<a href="UserLogin.php">USER</a>
+				<p/>
+				<a href="CustLogin.php">CUSTOMER</a>
+
+				
+         </center>
+
+    </body>
+</html>
---
+++ UserLogin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Tester</title>
+<!--    <link href="styles.css" rel="stylesheet" type="text/css" />		-->
+<!--	<script src = "Lab8.js" type ="text/javascript"></script>		-->
+</head>
+
+<body>
+ <center>
+    <h1> INSANE CROW'S USED CAR LOT </h1>
+                 <form id="lab2" action="confirmation.php" method="post">
+        <input type="submit" name="user" value="User" title="User login button"/> 
+        <input type="submit" name="customer" value="Customer" title="Customer button"/> 
+ 
+ </center>
+
+</body>
+</html>
