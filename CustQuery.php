10b71b440bb522049dd302a4aa2ad28a0334971c
Marten Yakob <martenyakob@live.ca>
2015-12-04 20:01:16 -0500

    r

---
+++ CustQuery.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Customer Page</title>
+
+<h1>Title</h1>
+
+<table width="200" border="1">
+  <tr>
+    <th scope="col">Pictures</th>
+    <th scope="col">Make</th>
+    <th scope="col">Model</th>
+    <th scope="col">Year</th>
+    <th scope="col">Colour</th>
+    <th scope="col">KM</th>
+  </tr>
+  <tr>
+    <td>test</td>
+    <td>test</td> 
+    <td>test</td> 
+    <td>test</td> 
+    <td>test</td>
+    <td>test</td>
+  </tr>
+  <tr>
+    <td>test</td>
+    <td>test</td> 
+    <td>test</td> 
+    <td>test</td> 
+    <td>test</td>
+    <td>test</td>
+  </tr>
+</table>
+</title>
+</head>
+
+<body>
+</body>
+</html>
+
+<!-- Albert wuz here -->
