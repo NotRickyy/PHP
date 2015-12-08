80de529843bb998bc6eaa9a5fb7aa385dc559b75
Albert Prezio <a.prezio13@gmail.com>
2015-12-07 23:49:08 -0500

    CSS changes/added Navigation bar support

--- UserLogin.php
+++ UserLogin.php
@@ -1,10 +1,24 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-		<title> Login </title>
-		<link rel="shortcut icon" href="Crow.jpg" />	
-        <h1> LOGIN</h1>
-    </head>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>CRAZY CROW'S</title>
+<link href="CSS/mainPage.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="JavaScript/Clock.js"></script>
+</head>
+
+<body>
+<div id="wrapper">
+  <header id="top">
+    <h1>CRAZY CROW'S USED CAR LOT</h1>
+    <nav id="mainnav">
+      <ul>
+        <li><a href="MainPage.php" class="thispage">Home</a></li>
+        <li><a href="CustQuery.php"> Customer</a></li>
+        <li><a href="#"> Admin</a></li>
+      </ul>
+    </nav>
+  </header>
     
     <body>
         <p>You are not logged in.</p>
