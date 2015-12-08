91267b12d6e07fc5099ce81632aab38468376890
Marten Yakob <martenyakob@live.ca>
2015-12-07 20:04:49 -0500

    Registar Page

---
+++ Registar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Login</title>
+<link rel="shortcut icon" href="Crow.jpg" />
+  <h1>Home page</h1>
+</head>
+
+<body>
+<p>Please Registar Below</p>
+  <label for="Username"> First Name:  &ensp; </label>
+  <input type="text" name="Firstname" id="FirstName"/>
+  <br/>
+  <label for="Username"> Last Name:  &ensp; </label>
+  <input type="text" name="Lastname" id="LastName"/>
+  <br/>
+  <label for="Username"> Username:  &ensp; </label>
+  <input type="text" name="Username" id="Username"/>
+  <br/>
+  <label for="Password"> Password:  &ensp; </label>
+  <input type="text" name="Password" id="Password"/>
+  <br/>
+  <label for="Username"> Confirm Password:  &ensp; </label>
+  <input type="text" name="CPassword" id="CPassword"/>
+  <br/>
+  <br/>
+  <input type="submit" name = "Register" value="Register New User"/>
+</body>
+</html>
